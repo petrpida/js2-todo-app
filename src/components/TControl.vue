a066c7e8f20cbed19c194f258f54e42e90839464
Petr Studeny <petstu@email.cz>
2022-06-04 19:02:11 +0200

    TControl added and used in PersonFormPage in v-for

---
+++ src/components/TControl.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-control">
+    <label :for="control">{{ label }}</label>
+    <input @input="onInput" :id="control" :type="type" :value="value" autocomplete="off" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TControl',
+    props: {
+        control: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            default: 'text'
+        },
+        value: {
+            validator: (v) => { typeof v === 'string' || typeof v === 'number' || v === null }
+        },
+        label: {
+            type: String
+        }
+    },
+    methods: {
+        onInput (event) {
+            this.$emit ('has-input', { value: event.target.value, control: this.control})
+        }
+    }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+.form-control
+  display: flex
+  flex-direction: column
+  text-align: left
+  margin-bottom: 2rem
+  
+.form-control input
+  font-size: 1.2rem
+  padding: .35em .75em
+
+</style>
--- src/views/PersonFormPage.vue
+++ src/views/PersonFormPage.vue
@@ -1,10 +1,20 @@
 <template>
   <h1>{{ title }}</h1>
 
-  <t-loading v-if="loading"/>
+  <t-loading v-if="loading" />
 
   <form v-else @submit="onSubmit">
-    <div class="form-control">
+    <t-control
+      v-for="item in controlKeys"
+      :key="item"
+      :control="item"
+      :type="controls[item].type"
+      :label="controls[item].label"
+      :value="person[item]"
+      @has-input="onHasInput"
+    />
+
+    <!-- <div class="form-control">
       <label for="first">first name</label>
       <input v-model="person.first" id="first" type="text" autocomplete="off" />
     </div>
@@ -41,13 +51,8 @@
     </div>
     <div class="form-control">
       <label for="phone">phone</label>
-      <input 
-        v-model="person.phone" 
-        id="phone" 
-        type="tel" 
-        autocomplete="off" 
-      />  
-    </div>
+      <input v-model="person.phone" id="phone" type="tel" autocomplete="off" />
+    </div> -->
     <t-button label="submit" />
   </form>
 </template>
@@ -56,6 +61,7 @@
 import db from "../utils/db.js";
 import TButton from "../components/TButton.vue";
 import TLoading from "../components/TLoading.vue";
+import TControl from "../components/TControl.vue";
 
 export default {
   name: "PersonFormPage",
@@ -69,22 +75,51 @@ export default {
         email: "",
         phone: "",
       },
-      loading: true
+      controls: {
+        first: {
+          type: "text",
+          label: "first name",
+        },
+        last: {
+          type: "text",
+          label: "last name",
+        },
+        email: {
+          type: "email",
+          label: "email",
+        },
+        phone: {
+          type: "tel",
+          label: "phone",
+        },
+        position: {
+          type: "text",
+          label: "position",
+        },
+        skills: {
+          type: "text",
+          label: "skills",
+        },
+      },
+      loading: true,
     };
   },
   created() {
     if (this.$route.params.id) {
       db.get("/persons/" + this.$route.params.id).then((record) => {
         this.person = record;
-        this.loading = false
+        this.loading = false;
       });
     }
-    this.loading = false
+    this.loading = false;
   },
   computed: {
     title() {
       return this.$route.params.id ? "edit person" : "add person";
     },
+    controlKeys () {
+      return Object.keys(this.controls)
+    }
   },
   methods: {
     onSubmit(e) {
@@ -101,14 +136,17 @@ export default {
         this.$router.push("/persons/" + this.$route.params.id);
       });
     },
+    onHasInput(payload) {
+      this.person[payload.control] = payload.value;
+    },
   },
-  components: { TButton, TLoading },
+  components: { TButton, TLoading, TControl },
 };
 </script>
 
 <style lang="stylus" scoped>
 
-// nebylo by vic DRY cely form udelat jako component? 
+// nebylo by vic DRY cely form udelat jako component?
 form
   width: 80%
   max-width: 500px
