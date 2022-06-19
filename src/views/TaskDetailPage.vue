f1101f7bf92672f0d1aa45e6ce39b198e3604a3c
Petr Studeny <petstu@email.cz>
2022-06-19 22:15:59 +0200

    ModalDelete added to TaskDetailPage

--- src/views/TaskDetailPage.vue
+++ src/views/TaskDetailPage.vue
@@ -3,7 +3,17 @@
   <t-loading v-if="loading" />
 
   <template v-else>
-    <t-modal :show="showAddPersonModal" @close-modal="showAddPersonModal = false">
+    <modal-delete
+      :showModal="showModalDelete"
+      question="Do you really want to delete task: "
+      :title="task.task"
+      @delete-clicked="onDeleteTaskClicked"
+      @cancel-clicked="showModalDelete = false"
+    />
+    <t-modal
+      :show="showAddPersonModal"
+      @close-modal="showAddPersonModal = false"
+    >
       <p>
         select person for task: <span class="text-bold">{{ task.task }}</span>
       </p>
@@ -29,15 +39,19 @@
       v-if="task.status === 'started'"
       label="mark as done"
       @clicked="onChangeStatus('done')"
-      :class="{'mr-1' : !personsInThisTask.length}"
+      :class="{ 'mr-1': !personsInThisTask.length }"
     />
     <t-button
       v-else-if="task.status === 'done'"
       label="mark as started"
       @clicked="onChangeStatus('started')"
-      :class="{'mr-1' : !personsInThisTask.length}"
+      :class="{ 'mr-1': !personsInThisTask.length }"
     />
-    <t-button v-if="!personsInThisTask.length" label="delete task" @clicked="onDeleteTaskClicked"/>
+    <t-button
+      v-if="!personsInThisTask.length"
+      label="delete task"
+      @clicked="showModalDelete = true"
+    />
 
     <ul class="detail-list">
       <li>
@@ -80,6 +94,7 @@
 import TLoading from "../components/TLoading.vue";
 import TButton from "../components/TButton.vue";
 import TModal from "../components/TModal.vue";
+import ModalDelete from "../components/ModalDelete.vue";
 import PersonList from "../components/PersonList.vue";
 import db from "../utils/db.js";
 import { formatDate } from "@/utils/dateUtils.js";
@@ -92,6 +107,7 @@ export default {
       loading: true,
       task: {},
       showAddPersonModal: false,
+      showModalDelete: false,
       allPersons: [],
       personsInThisTask: [],
     };
@@ -100,19 +116,20 @@ export default {
     dateToDisplay() {
       return formatDate(this.task.taskdate);
     },
-    personsInTaskIds () {
-      return this.personsInThisTask.map(item => item.personid)
+    personsInTaskIds() {
+      return this.personsInThisTask.map((item) => item.personid);
     },
-    allPersonsFiltered () {
-      return this.allPersons.filter(item => {
-        return this.personsInTaskIds.indexOf(item.id) < 0
-      })
-      .sort((a, b) => {
-        let aFull = a.last + a.first
-        let bFull = b.last + b.first
-        return aFull.localeCompare(bFull)
-      })
-    }
+    allPersonsFiltered() {
+      return this.allPersons
+        .filter((item) => {
+          return this.personsInTaskIds.indexOf(item.id) < 0;
+        })
+        .sort((a, b) => {
+          let aFull = a.last + a.first;
+          let bFull = b.last + b.first;
+          return aFull.localeCompare(bFull);
+        });
+    },
   },
   created() {
     const prom1 = db
@@ -126,11 +143,11 @@ export default {
   },
   methods: {
     fetchPersonsForThisTask() {
-      return db.get("personstasks?taskid=" + this.$route.params.taskid).then(
-        (data) => {
+      return db
+        .get("personstasks?taskid=" + this.$route.params.taskid)
+        .then((data) => {
           this.personsInThisTask = data;
-        }
-      );
+        });
     },
     onEditClicked() {
       this.$router.push("/taskformedit/" + this.$route.params.taskid);
@@ -164,13 +181,13 @@ export default {
         });
       });
     },
-    onDeleteTaskClicked () {
-      db.delete('tasks', {id: this.$route.params.taskid}).then(() => {
-        this.$router.push('/projects/' + this.task.projectid)
-      })
-    }
+    onDeleteTaskClicked() {
+      db.delete("tasks", { id: this.$route.params.taskid }).then(() => {
+        this.$router.push("/projects/" + this.task.projectid);
+      });
+    },
   },
-  components: { TLoading, TButton, TModal, PersonList },
+  components: { TLoading, TButton, TModal, PersonList, ModalDelete },
 };
 </script>
 
